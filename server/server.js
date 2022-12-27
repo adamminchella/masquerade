8b278a70969aa464b1fd0706529baf1dbd0b6802
adamminchella <adamminchella@msn.com>
2022-12-27 15:51:38 +0000

    Set up app to listen on http://localhost:3000

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const userRouter = require("./routers/user");
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Masquerade" });
+});
+
+server.use("/users", userRouter);
+
+module.exports = server;
